029414ad14f83460264bfe9f8d0b5c5483e2eae4
jhonnyalejo2212 <jhonnyalejo2212@gmail.com>
2019-07-09 17:16:53 -0500

    dasfd

--- src/views/master/Form.vue
+++ src/views/master/Form.vue
@@ -2,66 +2,172 @@
   <el-form :inline="true" :model="ruleForm" :rules="rules"
   label-position="top" ref="ruleForm"
   label-width="120px" class="" size="medium" autocomplete="off">
-    <el-form-item label="N° Master" prop="master" required>
-      <el-input v-model="ruleForm.master" placeholder="Número de Master"></el-input>
-    </el-form-item>
-    <el-form-item label="Fecha de liquidación" prop="liquidation_date" required>
-      <el-date-picker
-        v-model="ruleForm.liquidation_date"
-        type="date"
-        placeholder="Fecha de liquidación">
-      </el-date-picker>
-    </el-form-item>
-    <el-form-item label="Fecha de Master" prop="master_date" required>
-      <el-date-picker
-        v-model="ruleForm.master_date"
-        type="date"
-        placeholder="Fecha de Master">
-      </el-date-picker>
-    </el-form-item>
-    <el-form-item label="Tasa" prop="master_rate" required>
-      <el-input-number
-        v-model="ruleForm.master_rate"
-        placeholder="$ 0.00"
-        :precision="2"
-        :step="0.1" controls-position="right">
-      </el-input-number>
-    </el-form-item>
-    <el-form-item label="Peso Global" prop="global_weight" required>
-      <el-input-number
-        v-model="ruleForm.global_weight"
-        placeholder="0.00"
-        :precision="2"
-        :step="0.1" controls-position="right">
-      </el-input-number>
-    </el-form-item>
-    <el-form-item label="RATE" prop="rate" required>
-      <el-input-number
-        v-model="ruleForm.rate"
-        placeholder="$ 0.00"
-        :precision="2"
-        :step="0.1" controls-position="right">
-      </el-input-number>
-    </el-form-item>
-    <el-form-item label="Total Guias Hijas" prop="master_daughters" required>
-      <el-input-number
-        v-model="ruleForm.master_daughters"
-        placeholder="0"
-        controls-position="right">
-      </el-input-number>
-    </el-form-item>
-    <el-form-item label="N° Manifiesto" prop="manifest" required>
-      <el-input v-model="ruleForm.manifest" placeholder="Número de Manifiesto"></el-input>
-    </el-form-item>
-    <el-form-item label="Fecha de Manifiesto" prop="manifest_date" required>
-      <el-date-picker
-        v-model="ruleForm.manifest_date"
-        type="date"
-        placeholder="Fecha de Manifiesto">
-      </el-date-picker>
-    </el-form-item>
+  <el-row :gutter="20">
+    <el-col :span="16" class="pt-10">
+      <el-row type="flex" class="" justify="space-between">
+        <el-col :span="7">
+          <el-form-item label="N° Master" prop="master" required class="w-100">
+            <el-input v-model="ruleForm.master" placeholder="Número de Master"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="7">
+          <el-form-item label="Fecha de Master" prop="master_date" required class="w-100">
+            <el-date-picker
+              v-model="ruleForm.master_date"
+              type="date"
+              placeholder="Fecha de Master">
+            </el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col :span="7">
+          <el-form-item label="Fecha de liquidación" prop="liquidation_date" required class="w-100">
+            <el-date-picker
+              v-model="ruleForm.liquidation_date"
+              type="date"
+              placeholder="Fecha de liquidación">
+            </el-date-picker>
+          </el-form-item>
+        </el-col>
+      </el-row>
 
-    <el-form-item label="Sticker Declaración" prop="declaration_sticker">
+      <el-row type="flex" class="" justify="space-between">
+        <el-col :span="7">
+          <el-form-item label="TRM" prop="master_rate" required class="w-100">
+            <el-input-number
+              v-model="ruleForm.master_rate"
+              placeholder="$ 0.00"
+              :precision="2"
+              :step="0.1" controls-position="right">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+        <el-col :span="7">
+          <el-form-item label="Rate" prop="rate" required class="w-100">
+            <el-input v-model="ruleForm.rate" placeholder="Rate"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="7">
+          <el-form-item label="Peso Global" prop="global_weight" required class="w-100">
+            <el-input-number
+              v-model="ruleForm.global_weight"
+              placeholder="0.00"
+              :precision="2"
+              :step="0.1" controls-position="right">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row type="flex" class="" justify="space-between">
+        <el-col :span="7">
+          <el-form-item label="Total Guias Hijas" prop="master_daughters" required class="w-100">
+            <el-input-number
+              v-model="ruleForm.master_daughters"
+              placeholder="0"
+              controls-position="right">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+        <el-col :span="7">
+          <el-form-item label="Cliente" prop="client" class="w-100">
+            <el-input v-model="ruleForm.client" placeholder="Cliente"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="7">
+          <el-form-item label="Cobro" prop="payment" required class="w-100">
+            <el-input-number
+              v-model="ruleForm.payment"
+              placeholder="$ 0.00"
+              :precision="2"
+              :step="0.1" controls-position="right">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row type="flex" class="" justify="space-between">
+        <el-col :span="7">
+          <el-form-item label="N° Manifiesto" prop="manifest" required class="w-100">
+            <el-input v-model="ruleForm.manifest" placeholder="Número de Manifiesto"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="7">
+          <el-form-item label="Fecha de Manifiesto" prop="manifest_date" required class="w-100">
+            <el-date-picker
+              v-model="ruleForm.manifest_date"
+              type="date"
+              placeholder="Fecha de Manifiesto">
+            </el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col :span="7">
+          <el-form-item label="Diferencia" prop="difference" required class="w-100">
+            <el-input-number
+              v-model="ruleForm.difference"
+              placeholder="0.00"
+              :precision="2"
+              :step="0.1" controls-position="right">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-col>
+
+    <el-col :span="8"><div class="grid-content bg-green-aplex pt-10">
+      <el-row type="flex" class="" justify="space-around">
+        <el-col :span="16">
+          <el-form-item label="CIF" prop="cif" required class="w-100">
+            <el-input-number
+              v-model="ruleForm.cif"
+              placeholder="$ 0.00"
+              :precision="2"
+              :step="0.1" controls-position="right">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row type="flex" class="" justify="space-around">
+        <el-col :span="16">
+          <el-form-item label="Arancel" prop="rate" required class="w-100">
+            <el-input-number
+              v-model="ruleForm.rate"
+              placeholder="$ 0.00"
+              :precision="2"
+              :step="0.1" controls-position="right">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row type="flex" class="" justify="space-around">
+        <el-col :span="16">
+          <el-form-item label="IVA" prop="iva" required class="w-100">
+            <el-input-number
+              v-model="ruleForm.iva"
+              placeholder="% 0.00"
+              :precision="2"
+              :step="0.1" controls-position="right">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row type="flex" class="" justify="space-around">
+        <el-col :span="16">
+          <el-form-item label="Total" prop="total" required class="w-100">
+            <el-input-number
+              v-model="ruleForm.total"
+              placeholder="$ 0.00"
+              :precision="2"
+              :step="0.1" controls-position="right">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </div></el-col>
+  </el-row>
+    <!-- <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24"> -->
+
+    <!-- </el-col> -->
+    <!-- <el-form-item label="Sticker Declaración" prop="declaration_sticker">
       <el-input v-model="ruleForm.declaration_sticker" placeholder="Sticker Declaración"></el-input>
     </el-form-item>
     <el-form-item label="Fecha de Declaración" prop="declaration_date">
@@ -70,22 +176,20 @@
         type="date"
         placeholder="Fecha de Declaración">
       </el-date-picker>
-    </el-form-item>
-
-    <el-form-item label="CIFT" prop="rate" required>
-      <el-input-number
-        v-model="ruleForm.rate"
-        placeholder="$ 0.00"
-        :precision="2"
-        :step="0.1" controls-position="right">
-      </el-input-number>
-    </el-form-item>
-
-
-    <el-form-item style="width: 100%;">
-      <el-button type="primary" @click="submitForm('ruleForm')">Create</el-button>
-      <el-button @click="resetForm('ruleForm')">Reset</el-button>
-    </el-form-item>
+    </el-form-item> -->
+    <el-row :gutter="20">
+      <el-col :span="16" class="pt-10">
+        <el-row type="flex" justify="space-between">
+          <el-col :span="12">
+            <el-form-item style="">
+              <el-button type="primary" @click="submitForm('ruleForm')">Create</el-button>
+              <el-button @click="resetForm('ruleForm')">Reset</el-button>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12"></el-col>
+        </el-row>
+      </el-col>
+    </el-row>
   </el-form>
 </template>
 
@@ -148,4 +252,33 @@ export default {
 </script>
 
 <style lang="css" scoped>
+  .pt-10{
+    padding-top: 10px;
+  }
+  .w-25{
+    width: 25%
+  }
+  .w-50{
+    width: 50%
+  }
+  .w-100{
+    width: 100%
+  }
+  .el-date-editor.el-input, .el-date-editor.el-input__inner, .el-input-number, .el-input{
+    width: 100%
+  }
+  .el-row {
+    margin-bottom: 0px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .bg-green-aplex {
+    background: #F0F9EB;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+
 </style>
